42909334bb5df4303e9e04fb425a9109439f3965
Andrew Haupt <ajhaupt7@gmail.com>
2018-05-12 12:40:54 -0700

    add tailwind, postcss config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,25 @@
-module.exports = {
+const PurgecssPlugin = require('purgecss-webpack-plugin')
+const glob = require('glob-all')
+const path = require('path')
+
+class PurgeFromTailwind {
+  static extract(content) {
+    // Need to make sure we match responsive classes,
+    // i.e. lg:w-full
+    return content.match(/[A-z0-9-:\/]+/g) || [];
+  }
+}
+
+const config = {
   /*
   ** Headers of the page
   */
   head: {
-    title: 'bendjs_demos',
+    title: 'BendJS Demos',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'BendJS Demos' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -21,6 +33,10 @@ module.exports = {
   ** Build configuration
   */
   build: {
+    postcss: [
+      require('tailwindcss')('./tailwind.js'),
+      require('autoprefixer')
+    ],
     /*
     ** Run ESLint on save
     */
@@ -33,6 +49,31 @@ module.exports = {
           exclude: /(node_modules)/
         })
       }
+      if (!isDev) {
+        // Remove unused CSS using purgecss. See https://github.com/FullHuman/purgecss
+        // for more information about purgecss.
+        config.plugins.push(
+          new PurgecssPlugin({
+            paths: glob.sync([
+              path.join(__dirname, './pages/**/*.vue'),
+              path.join(__dirname, './layouts/**/*.vue'),
+              path.join(__dirname, './components/**/*.vue')
+            ]),
+            whitelist: ['html', 'body'],
+            extractors: [
+              {
+                extractor: PurgeFromTailwind,
+                extensions: ['vue']
+              }
+            ]
+          })
+        )
+      }
     }
-  }
+  },
+  css: [
+    { src: '~assets/css/app.scss', lang: 'scss' }
+  ],
 }
+
+module.exports = config;
