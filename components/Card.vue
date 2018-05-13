1f746c51d786f74b7d376b2b46144bf40dfddced
Andrew Haupt <ajhaupt7@gmail.com>
2018-05-13 12:32:58 -0700

    add card component

---
+++ components/Card.vue
@@ -0,0 +1,61 @@
+<template>
+  <div :class="cardClass">
+    <span class="absolute pin-l pin-t p-4">{{ suit }}</span>
+    <span class="text-lg">{{ name }}</span>
+    <span class="absolute pin-r pin-b p-4 text-right">{{ suit }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'card',
+  props: {
+    suit: String,
+    name: String,
+    id: Number,
+    value: Number
+  },
+  computed: {
+    cardClass() {
+      return [
+        'card',
+        'flex',
+        'justify-center',
+        'items-center',
+        'shadow-md',
+        'rounded',
+        'bg-white',
+        'p-2',
+        { 'text-black': this.suit === '♠' || this.suit ===  '♣' },
+        { 'text-red': this.suit === '♥' || this.suit ===  '♦' }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+  width: 150px;
+  height: 200px;
+  position: relative;
+}
+
+.card__suit {
+  width: 100%;
+  display: block;
+}
+
+.card__suit--bottom {
+  transform: rotate(180deg);
+}
+
+.card__number {
+  width: 100%;
+  position: absolute;
+  top: 38%;
+  text-align: center;
+}
+
+</style>
+
