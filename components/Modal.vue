ea1b2a121f16bacd6f716041175c7777c92f9e20
Andrew Haupt <ajhaupt7@gmail.com>
2018-05-13 12:33:04 -0700

    add modal component

--- components/Modal.vue
+++ components/Modal.vue
@@ -1,33 +1,31 @@
 <template>
-  <div class="modal">
+  <div class="p-8 flex flex-col justify-between items-center rounded shadow-md">
+    <a class="close text-red cursor-pointer shadow-md" @click="handleCloseClick">
+      <x-icon></x-icon>
+    </a>
   	<h2>Hello plz subscribe to newzletter.</h2>
 		<slot></slot>
   </div>
 </template>
 
-<style lang="scss" scoped>
-.modal {
-  background-image: url('https://media.giphy.com/media/jkSvCVEXWlOla/giphy.gif');
-  background-size: cover;
-  background-repeat: no-repeat;
-  color: black;
-  padding: 20px;
-  width: 80%;
-  height: 500px;
-  max-height: 80%;
-  max-width: 800px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate3d(-50%, -50%, 0);
-  border-radius: 4px;
-  box-shadow: 2px 6px 12px rgba(0,0,0,0.1);
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-}
+<script>
+import { XIcon } from 'vue-feather-icons'
 
+export default {
+  name: 'modal',
+  components: {
+    XIcon
+  },
+  methods: {
+    handleCloseClick() {
+      this.$emit('close-click')
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
 .close {
   position: absolute;
   top: -10px;
@@ -35,13 +33,10 @@
   width: 40px;
   height: 40px;
   background: white;
-  color: rgb(228, 15, 15);
-  cursor: pointer;
   border-radius: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
-  box-shadow: 2px 6px 12px rgba(0,0,0,0.1);
   border: 2px solid rgba(black, 0.2);
 }
 </style>
