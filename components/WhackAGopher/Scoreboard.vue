af317851eacc6e29903efc439e6708cec6ef6423
Andrew Haupt <ajhaupt7@gmail.com>
2018-05-14 12:47:26 -0700

    add scoreboard component

---
+++ components/WhackAGopher/Scoreboard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-if="this.score || this.score === 0" class="scoreboard">
+    <div :class="messageClasses" v-if="message">{{ message }}</div>
+    <div>{{ score }}</div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'scoreboard',
+  props: {
+    message: String,
+    messageClasses: String
+  },
+  computed: {
+    ...mapState('GameEngine', [
+      'score'
+    ])
+  }
+}
+</script>
