1078eba987fc2600001cef045df5f264dbe66030
Andrew Haupt <ajhaupt7@gmail.com>
2018-05-14 12:47:37 -0700

    add timer component

---
+++ components/WhackAGopher/Timer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="timer">
+    <div :class="messageClasses" v-if="message">{{ message }}</div>
+    <div>{{ time }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'timer',
+  props: {
+    time: Number,
+    message: String,
+    messageClasses: String
+  }
+}
+</script>
